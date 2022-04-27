61850617a33e933c313e0a96bd94dde29c3c62b1
Mehmet Oya <moya@MagiClick.com>
2022-04-27 15:20:06 +0300

    clientService, publisher and event add

---
+++ Watermark/Services/RabbitMQPublisher.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Watermark.Services
+{
+    public class RabbitMQPublisher
+    {
+        private readonly RabbitMQClientService _rabbitMQClientService;
+        public RabbitMQPublisher(RabbitMQClientService rabbitMQClientService)
+        {
+            _rabbitMQClientService = rabbitMQClientService;
+        }
+        public void Publish(ProductImageCreatedEvent productImageCreatedEvent) 
+        {
+            var channel = _rabbitMQClientService.Connect();
+            var bodyString = JsonSerializer.Serialize(productImageCreatedEvent);
+            var bodyByte = Encoding.UTF8.GetBytes(bodyString);
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+
+            channel.BasicPublish(exchange:RabbitMQClientService.ExcangeName,routingKey:RabbitMQClientService.RoutingWatermark,
+                basicProperties:properties,body:bodyByte);
+        }
+    }
+}
